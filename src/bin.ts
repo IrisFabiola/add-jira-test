f20bf88a2b6035e0b254e0e02c9e5c3b73920dd2
IrisFabiola <iriscarvalho235@gmail.com>
2023-04-02 19:03:31 +0100

    refactor: extract issue logic

--- src/bin.ts
+++ src/bin.ts
@@ -1,14 +1,11 @@
 #!/usr/bin/env -S ts-node --files
 
 import * as dotenv from 'dotenv';
-import { readFileSync } from 'fs';
 import { writeFile } from 'fs/promises';
-import Mustache from 'mustache';
-import { groupby, map } from 'itertools';
 import { Version3Client } from 'jira.js';
-import { type Document } from 'jira.js/out/version3/models';
 import minimist from 'minimist';
 import path from 'path';
+import { createTestFromIssue } from './issue';
 
 dotenv.config();
 
@@ -35,76 +32,7 @@ const client = new Version3Client({
 		issueIdOrKey: key,
 	});
 
-	const { fields } = issue;
-
-	const { description, summary } = fields;
-
-	if (!description?.content) {
-		throw new Error('ticket does not have description');
-	}
-
-	const parsed = parseContent(description.content);
-
-	const issueTemplateFile = readFileSync(
-		path.resolve(__dirname, './test.mustache'),
-	).toString();
-
-	const issueTest = Mustache.render(issueTemplateFile, {
-		key,
-		title: summary,
-		...parsed,
-	});
+	const issueTest = await createTestFromIssue(issue, client);
 
 	await writeFile(path.resolve(process.cwd(), fileName), issueTest);
 })();
-
-function parseContent(content: Array<Omit<Document, 'version'>>) {
-	const iterable = content[Symbol.iterator]();
-
-	const grouped = groupby(generateStatements(iterable), ([type]) => type);
-
-	const statementsByType = new Map(
-		map(grouped, ([type, typedStatements]) => [
-			type,
-			map(typedStatements, ([, statement]) => statement),
-		]),
-	);
-
-	return {
-		given: statementsByType.get('GIVEN') ?? [],
-		when: statementsByType.get('WHEN') ?? [],
-		then: statementsByType.get('THEN') ?? [],
-	};
-}
-
-function* generateStatements(
-	iter: IterableIterator<Omit<Document, 'version'>>,
-) {
-	let type = '';
-
-	for (const value of iter) {
-		if (!type && value.type !== 'heading') {
-			continue;
-		}
-
-		const isInLastSection = type === 'THEN';
-
-		if (value.type === 'heading' && isInLastSection) {
-			return;
-		}
-
-		if (
-			value.type === 'heading' &&
-			value.content?.[0].text &&
-			['GIVEN', 'WHEN', 'THEN'].includes(value.content[0].text)
-		) {
-			type = value.content[0].text;
-			continue;
-		}
-
-		if (value.type === 'paragraph') {
-			yield [type, value.content?.map((c) => c.text).join('')] as const;
-			continue;
-		}
-	}
-}
---
+++ src/issue.ts
@@ -0,0 +1,82 @@
+import { readFileSync } from 'fs';
+import Mustache from 'mustache';
+import { groupby, map } from 'itertools';
+import path from 'path';
+import { type Version3Client } from 'jira.js';
+import { type Issue, type Document } from 'jira.js/out/version3/models';
+
+export async function createTestFromIssue(
+	issue: Issue,
+	client: Version3Client,
+) {
+	const { fields } = issue;
+
+	const { description, summary } = fields;
+
+	if (!description?.content) {
+		throw new Error('ticket does not have description');
+	}
+
+	const parsed = parseContent(description.content);
+
+	const issueTemplateFile = readFileSync(
+		path.resolve(__dirname, './test.mustache'),
+	).toString();
+
+	return Mustache.render(issueTemplateFile, {
+		key: issue.key,
+		title: summary,
+		...parsed,
+	});
+}
+
+function parseContent(content: Array<Omit<Document, 'version'>>) {
+	const iterable = content[Symbol.iterator]();
+
+	const grouped = groupby(generateStatements(iterable), ([type]) => type);
+
+	const statementsByType = new Map(
+		map(grouped, ([type, typedStatements]) => [
+			type,
+			map(typedStatements, ([, statement]) => statement),
+		]),
+	);
+
+	return {
+		given: statementsByType.get('GIVEN') ?? [],
+		when: statementsByType.get('WHEN') ?? [],
+		then: statementsByType.get('THEN') ?? [],
+	};
+}
+
+function* generateStatements(
+	iter: IterableIterator<Omit<Document, 'version'>>,
+) {
+	let type = '';
+
+	for (const value of iter) {
+		if (!type && value.type !== 'heading') {
+			continue;
+		}
+
+		const isInLastSection = type === 'THEN';
+
+		if (value.type === 'heading' && isInLastSection) {
+			return;
+		}
+
+		if (
+			value.type === 'heading' &&
+			value.content?.[0].text &&
+			['GIVEN', 'WHEN', 'THEN'].includes(value.content[0].text)
+		) {
+			type = value.content[0].text;
+			continue;
+		}
+
+		if (value.type === 'paragraph') {
+			yield [type, value.content?.map((c) => c.text).join('')] as const;
+			continue;
+		}
+	}
+}
